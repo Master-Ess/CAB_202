e745336e33ba000bc71580394ea293349d3be133
Master-Ess <67045069+Master-Ess@users.noreply.github.com>
2021-10-16 21:46:13 +1000

    Update Kenzie_Haigh_202-MASTER.c

--- Kenzie_Haigh_202-MASTER.c
+++ Kenzie_Haigh_202-MASTER.c
@@ -302,11 +302,11 @@ void question_cycle(void){
             }
 
             if (response_b == 2){
-            if (BIT_IS_SET(PINC, 0)){
+            if (BIT_IS_SET(PINC, 3)){
                 response_b = 0;
                 uart_put_string("You 2");
             }
-            if (BIT_IS_SET(PINC, 1)){
+            if (BIT_IS_SET(PINC, 2)){
                 response_b = 1;
                 uart_put_string("Me 1");
             }
